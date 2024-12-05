6bd10157bdbc387765fc79316a635e1ad5f00b82
Atul Gupta <dev.atul2229@outlook.com>
2024-12-05 21:26:05 +0530

    updated

--- services/constants.py
+++ services/constants.py
@@ -1,5 +1,8 @@
 class Constants:
-    db_path=""
-    
+    db_url = "sqlite:///txt2sql.db"
+    organization='org-0WiJlDAVVOIh4oaisiP49G17'
+    project='proj_gWaFdgg8GJTA5FX404f8ooWO'
+    api_key='sk-esgate-openai-service-id-dev-bs9EG2Y0QBja7WGt30QbT3BlbkFJImJ2oS36x8cbc90agRj2'
+    assistant_id="asst_nVrytkzJcAMWfho5FkQpl6yL"
     
 const=Constants
